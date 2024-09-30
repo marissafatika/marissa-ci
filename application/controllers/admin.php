b259c868de1885a7174f253632faafec1b0126b5
marissafatika <sarimarissa867@gmail.com>
2024-09-30 10:23:59 +0700

    tmbhedit

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -28,8 +28,7 @@ class Admin extends CI_Controller {
 		//memanggil siswa model//
 		$this->load->model('siswa_model');
 		$data['edit']=$this->siswa_model->getSiswaByNis($nis);
-
-		$data['alamat']=['batang','pekalongan','pemalang','tegal'];
+		$data['alamat']=[1, 2, 3, 4];
 
 		$this->load->view('templete/header');
 		$this->load->view('edit_siswa',$data);
@@ -40,7 +39,7 @@ class Admin extends CI_Controller {
 		$data =
 		[
 			"nama" => $this->input->post('nama',true),
-			"alamat" => $this->input->post('alamat',true),
+			"id_alamat" => $this->input->post('alamat',true),
 			"no_telp" => $this->input->post('no_telp',true),
 	    ];
 
--- application/views/edit_siswa.php
+++ application/views/edit_siswa.php
@@ -30,7 +30,7 @@
         <option value="">--pilih alamt--</option>
 
         <?php foreach($alamat as $al) : ?>
-            <?php if($al == $edit['alamat']): ?>
+            <?php if($al == $edit['id_alamat']): ?>
                 <option value="<?= $al; ?>" selected><?= $al; ?></option>
                 <?php else : ?>
                 <option value="<?= $al; ?>"><?= $al; ?></option>
